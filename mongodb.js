3e14cf07d2374f4e2222a8d16935d7047b840bae
ahkian <dbrazel4@gmail.com>
2020-10-21 10:42:09 -0500

    Removed code for adding documents

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,11 @@
 const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-app'
+
  MongoClient.connect(connectionURL, {useNewUrlParser: true},
    (error, client) => {
    if(error) {
@@ -10,8 +13,10 @@ const databaseName = 'task-app'
    }
    const db = client.db(databaseName)
 
-   db.collection('users').insertOne({
-     name: "Daniel",
-     age: 26
+   db.collection('users').findOne({name: "Jesus"}, (error, user) => {
+     if(error) {
+       return console.log(error);
+     }
+     console.log(user)
    })
  })
